641a75ce575eaa72fe16a4e390c03f8391a1e391
lingyq <lyq.roco@foxmail.com>
2018-12-19 16:13:34 +0800

    ch6

--- Template/USER/main.c
+++ Template/USER/main.c
@@ -1,33 +1,18 @@
-#include "stm32f10x.h"
- void Delay(u32 count)
- {
-  u32 i=0;
-  for(;i<count;i++);
+#include "led.h"
+#include "delay.h"
+#include "sys.h"
 
- }
  int main(void)
  {	
-  GPIO_InitTypeDef  GPIO_InitStructure;
- 	
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA|RCC_APB2Periph_GPIOD, ENABLE);
-	
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-  GPIO_Init(GPIOA, &GPIO_InitStructure);
-  GPIO_SetBits(GPIOA,GPIO_Pin_8);
-
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
-  GPIO_Init(GPIOD, &GPIO_InitStructure);
-  GPIO_SetBits(GPIOD,GPIO_Pin_2);
-  while(1)
+	delay_init();
+	LED_Init();
+	while(1)
 	{
-	    GPIO_ResetBits(GPIOA,GPIO_Pin_8);
-	    GPIO_SetBits(GPIOD,GPIO_Pin_2);
-		Delay(3000000);
+		GPIO_ResetBits(GPIOA,GPIO_Pin_8);
+		GPIO_SetBits(GPIOD,GPIO_Pin_2);
+		delay_ms(300);
 		GPIO_SetBits(GPIOA,GPIO_Pin_8);
 		GPIO_ResetBits(GPIOD,GPIO_Pin_2);
-		Delay(3000000);
+		delay_ms(300);
 	}
- }
-
+}
