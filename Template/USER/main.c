3b3141f0af3119746e04714142e271b2e3cdb2ae
lingyq <lyq.roco@foxmail.com>
2018-12-19 16:18:30 +0800

    ch7

--- Template/USER/main.c
+++ Template/USER/main.c
@@ -1,18 +1,31 @@
 #include "led.h"
 #include "delay.h"
 #include "sys.h"
-
+#include "key.h"
  int main(void)
  {	
+	u8 t=0;	  
 	delay_init();
 	LED_Init();
+	KEY_Init();
+	LED0=0;
 	while(1)
 	{
-		GPIO_ResetBits(GPIOA,GPIO_Pin_8);
-		GPIO_SetBits(GPIOD,GPIO_Pin_2);
-		delay_ms(300);
-		GPIO_SetBits(GPIOA,GPIO_Pin_8);
-		GPIO_ResetBits(GPIOD,GPIO_Pin_2);
-		delay_ms(300);
-	}
+		t=KEY_Scan(0);
+		switch(t)
+		{				 
+			case KEY0_PRES:
+				LED0=!LED0;
+				break;
+			case KEY1_PRES:
+				LED1=!LED1;
+				break;
+			case WKUP_PRES:				
+				LED0=!LED0;
+				LED1=!LED1;
+				break;
+			default:
+				delay_ms(10);	
+		} 
+	}		 
 }
